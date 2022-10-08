8422ff0681325ec11d3e1882a32cafd407c38333
Washington Charles <toncharles@live.com>
2022-10-08 15:37:20 -0300

    Setting new transaction modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,34 @@
+import {useState} from 'react';
+import Modal from 'react-modal';
+
 import logoImg from '../../assets/logo.svg';
 import { Container, Content} from './styles';
 
 export function Header() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dtMoney" />
-        <button type="button">
+        <button type="button" onClick={handleOpenNewTransactionModal}>
           Nova Transação
         </button>
+
+        <Modal 
+          isOpen={isNewTransactionModalOpen} 
+          onRequestClose={handleCloseNewTransactionModal}
+        >
+          <h2>Cadastrar Transação</h2>
+        </Modal>
       </Content>
     </Container>
   )
