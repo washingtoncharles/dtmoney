572df58833be254d6ad1ac09a279f7e71c13d11a
Washington Charles <toncharles@live.com>
2022-10-09 13:36:40 -0300

    Creating Type Buttons

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,9 @@
 import Modal from 'react-modal';
-import { Container } from './styles';
+import { Container, TransactionTypeContainer } from './styles';
+
+import closeImg from '../../assets/close.svg';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -14,11 +18,29 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
       overlayClassName="react-modal-overlay"
       className="react-modal-content"
     >
+
+      <button type='button' onClick={onRequestClose} className="react-modal-close">
+        <img src={closeImg} alt="Fechar Transação" />
+      </button>
+
       <Container>
         <h2>Cadastrar Transação</h2>
 
         <input placeholder='Descrição' />
         <input type="number" placeholder='Valor' />
+
+        <TransactionTypeContainer>
+          <button type='button'> {/*inicio - botao de tipo do lancamento - entrada*/}
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span> 
+          </button> {/*final - botao de tipo do lancamento - entrada*/}
+
+          <button type='button'> {/*botao de tipo do lancamento - saida*/}
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </button> {/*final - botao de tipo do lancamento - saida*/}
+        </TransactionTypeContainer>
+
         <input placeholder='Categoria' />
 
         <button type="submit">Cadastrar</button>
--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,3 +1,84 @@
 import styled from "styled-components";
 
-export const Container = styled.form``
+export const Container = styled.form`
+  h2 {
+    color: var(--text-title);
+    font-size: 1.5rem;
+    margin-bottom: 2rem;
+  }
+
+  input {
+    width: 100%;
+    padding: 0 1.5rem;
+    height: 4rem;
+    border-radius: 0.25rem;
+
+    border: 1px solid #D7D7D7;
+    background: #E7E9EE;
+
+    font-weight: 400;
+    font-size: 1rem;
+
+    &::placeholder {
+      color: var(--text-body);
+    }
+
+    & + input {
+      margin-top: 1rem;
+    }
+  }
+
+  button[type="submit"] {
+    width: 100%;
+    padding: 0 1.5rem;
+    height: 4rem;
+    background: var(--green);
+    color: #FFFFFF;
+    border-radius: 0.25rem;
+    border: 0;
+    font-size: 1rem;
+    margin-top: 1.5rem;
+    font-weight: 600;
+
+    transition: filter 0.2s;
+
+    &:hover {
+      filter: brightness(0.9);
+    }
+  }
+`;
+
+export const TransactionTypeContainer = styled.div`
+  margin: 1rem 0;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 0.5rem;
+
+  button {
+    height: 4rem;
+    border: 1px solid #D7D7D7;
+    border-radius: 0.25rem;
+
+    background: transparent;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    &:hover {
+      border-color: #AAAAAA;
+    }
+
+    img {
+      width: 24px;
+      height: 24px;
+    }
+
+    span {
+      display: inline-block;
+      margin-left: 1rem;
+      font-size: 1rem;
+      color: var(--text-title);
+    }
+  }
+`
